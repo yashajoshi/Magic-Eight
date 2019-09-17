bb6b1cf1e4cb2048c5f55e1c41c362d44fbb3969
Yashaswini <yjoshi@umich.edu>
2019-09-17 16:51:18 -0400

    added main function

--- HW3/magic_eight.py
+++ HW3/magic_eight.py
@@ -1,3 +1,14 @@
 def ask_question():
 	question = input("What is your question?\n")
 	return question
+    
+if __name__ == "__main__":
+	while True:
+		question = ask_question()
+		if question == "quit":
+			break
+		if question[-1]!='?':
+			print("I’m sorry, I can only answer questions")
+		else:
+			print(8ball_answer())
+			break
