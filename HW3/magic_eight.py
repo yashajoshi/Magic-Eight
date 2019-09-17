c2d24f90bb35cbab138b5e0e93bc18c2e1c98f8b
Jonathan Martindale <martijal@umich.edu>
2019-09-17 16:39:44 -0400

    Added function to select answers

--- HW3/magic_eight.py
+++ HW3/magic_eight.py
@@ -1,3 +1,12 @@
 def ask_question():
 	question = input("What is your question?\n")
 	return question
+
+possible_answers = ['It is certain.', 'it is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.',
+                    'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.',
+                    "Don't count on it.", "My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
+
+
+def 8ball_answer():
+        x = random.choice(possible_answers)
+        return x
